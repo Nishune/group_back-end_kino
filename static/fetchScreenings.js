71e2de23b8b8c255f745d169abc17330485f6d43
Rikard Engström <rikardengstrom89@gmail.com>
2025-02-06 15:36:20 +0100

    fix

--- static/fetchScreenings.js
+++ static/fetchScreenings.js
@@ -1,8 +1,8 @@
 (async () => {
-  const screeningsIDList = document.querySelector(".screeningsID-list");
+  const screeningsIDList = document.querySelector('.screeningsID-list');
   const parts = window.location.pathname.split('/');
   const movieId = parts.length > 2 ? parts[2] : null;
-  const API_URL = `http://localhost:5080/api/screenings/${movieId}`
+  const API_URL = `/api/screenings/${movieId}`;
 
   if (!screeningsIDList) {
     return;
@@ -20,32 +20,31 @@
     }
 
     const data = await response.json();
-   
+
     if (data.length === 0) {
-      const li = document.createElement("li");
-      li.textContent = "För tillfället har vi inga visningar på denna film."
-      li.classList.add("screeningsID-listItem");
+      const li = document.createElement('li');
+      li.textContent = 'För tillfället har vi inga visningar på denna film.';
+      li.classList.add('screeningsID-listItem');
       screeningsIDList.append(li);
       return;
     }
 
-    data.forEach(movie => {
+    data.forEach((movie) => {
       const date = new Date(movie.start_time);
-      const formattedDate = date.toLocaleString("sv-SE", { 
-        year: "numeric", 
-        month: "2-digit", 
-        day: "2-digit", 
-        hour: "2-digit", 
-        minute: "2-digit" 
+      const formattedDate = date.toLocaleString('sv-SE', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
       });
-      const li = document.createElement("li");
-      li.textContent = `${movie.room} | ${formattedDate}`
-      li.classList.add("screeningsID-listItem") 
-      
+      const li = document.createElement('li');
+      li.textContent = `${movie.room} | ${formattedDate}`;
+      li.classList.add('screeningsID-listItem');
+
       screeningsIDList.append(li);
     });
   } catch (e) {
     console.error(e);
   }
-
-})()
+})();
